fce1fceb84f7f342c3b0c9e732d635aea02cbc10
Brady Love <brady@artemis.net>
2016-08-29 22:37:01 -0600

    dotter add command works with multipile files

--- cmd/add.go
+++ cmd/add.go
@@ -18,7 +18,7 @@ push changes to your dot files to the remote repository`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
-			fmt.Println("requires filepath(s)")
+			fmt.Println("filepath(s) required")
 			os.Exit(1)
 		}
 
@@ -26,9 +26,11 @@ push changes to your dot files to the remote repository`,
 
 		manager := newFileManager()
 
-		if err := manager.Add(args[0]); err != nil {
-			fmt.Printf("Failed to add file to your dotfiles: %s\n", err)
-			os.Exit(1)
+		for _, fp := range args {
+			if err := manager.Add(fp); err != nil {
+				fmt.Printf("Failed to add file to your dotfiles: %s\n", err)
+				os.Exit(1)
+			}
 		}
 
 		fmt.Println("Done.")
